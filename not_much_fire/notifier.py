35365517bdd969fb767960df0d65c4b08d1a8d92
Alexandr Skurikhin <a.askurihin@sprinthost.ru>
2020-11-27 15:16:48 +0300

    Add MacOS support

--- not_much_fire/notifier.py
+++ not_much_fire/notifier.py
@@ -2,10 +2,7 @@ from email.utils import parseaddr
 
 from notmuch import Message
 
-import notify2
-
-
-class Notifier:
+class Notifi2Notifier:
     def __init__(self, app_name: str):
         """Initialize the connection to the notification daemon.
 
@@ -29,3 +26,32 @@ class Notifier:
         subject = message.get_header("subject")
         notification = notify2.Notification(sender, subject)
         notification.show()
+
+class PyncNotifier:
+    def __init__(self, app_name: str):
+        pass
+
+
+    def notify_message(self, message: Message) -> None:
+        """Formats the message into a notification and send it to the daemon.
+
+        Uses the name of the sender as notification title. In case such
+        is not defined, the mail address is used instead. As body of the
+        notification the message subject is used. The formatted message
+        is send to the systems notification daemon.
+        """
+        import pync
+        name, address = parseaddr(message.get_header("from"))
+        sender = name or address
+        subject = message.get_header("subject")
+        pync.notify(title=sender, message=f"'{subject}'")
+
+
+def Notifier(app_name: str):  # noqa
+    try:
+        import notify2
+        return Notifi2Notifier(app_name)
+    except ImportError:
+        import pync
+        return PyncNotifier(app_name)
+
